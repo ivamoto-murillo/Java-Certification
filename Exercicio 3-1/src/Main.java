b2e7caeac3ca78a77a12eabb52c12c0fdef0b8a4
Murillo Ivamoto <ivamoto.murillo@gmail.com>
2015-02-20 13:30:02 -0200

    Adiciona projeto referente ao exercício 3-1

---
+++ Exercicio 3-1/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	
+	public static void main(String [] args) {
+		float floatNumber = 234.56F;
+		short shortNumber = (short) floatNumber;
+		
+		System.out.println("Valor de floatNumber -> " + floatNumber);
+		System.out.println("Valor de shortNumber -> " + shortNumber);
+	}
+	
+}
