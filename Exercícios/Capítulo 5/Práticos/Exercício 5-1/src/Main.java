d5e66f333a8df3190df92cade856c7acf33202b7
Murillo Ivamoto <ivamoto.murillo@gmail.com>
2015-03-31 22:20:46 -0300

    Adiciona exercício 5-1

---
+++ Exercícios/Capítulo 5/Práticos/Exercício 5-1/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+	public static void main(String[] args) {
+		char c = 'b';
+		
+		switch (c) {
+			case 'z': {
+				System.out.println("É a letra z!");
+				break;
+			}
+			case 'm': {
+				System.out.println("É a letra m!");
+				break;
+			}
+			default: {
+				System.out.println("Default maroto!");
+				break;
+			}
+			case 'a': {
+				System.out.println("É a letra a!");
+				break;
+			}
+		}
+	}
+
+}
